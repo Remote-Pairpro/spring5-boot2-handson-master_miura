57c0a42636b5cf369e10f69d662cd281c80de58c
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-08-25 15:18:18 +0900

    TODO 2-04

--- 01-spring5/src/main/java/com/example/service/impl/CustomerServiceImpl.java
+++ 01-spring5/src/main/java/com/example/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,10 @@ package com.example.service.impl;
 import com.example.persistence.entity.Customer;
 import com.example.persistence.repository.CustomerRepository;
 import com.example.service.CustomerService;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRED;
 
 // TODO 2-02 ビジネスロジッククラスのBeanであることを示すアノテーションを付加する
 
@@ -14,8 +18,7 @@ public class CustomerServiceImpl implements CustomerService {
         this.repository = repository;
     }
 
-    // TODO 2-04 トランザクション管理アノテーションを付加する（伝播属性＝REQUIRED, 読み取り専用＝true）
-
+    @Transactional(propagation = REQUIRED, readOnly = true)
     @Override
     public Iterable<Customer> findAll() {
         // TODO 2-05 CustomerRepositoryのfindAll()を呼び出す
