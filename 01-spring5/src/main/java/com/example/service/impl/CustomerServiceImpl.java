b68e830b9ae3fdd2ed88c172439e8c1ce445b507
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-08-25 15:14:55 +0900

    TODO 2-03

--- 01-spring5/src/main/java/com/example/service/impl/CustomerServiceImpl.java
+++ 01-spring5/src/main/java/com/example/service/impl/CustomerServiceImpl.java
@@ -3,17 +3,16 @@ package com.example.service.impl;
 import com.example.persistence.entity.Customer;
 import com.example.persistence.repository.CustomerRepository;
 import com.example.service.CustomerService;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 // TODO 2-02 ビジネスロジッククラスのBeanであることを示すアノテーションを付加する
 
 public class CustomerServiceImpl implements CustomerService {
 
-    // TODO 2-03 CustomerRepositoryをコンストラクタインジェクションする
+    private final CustomerRepository repository;
 
-
+    public CustomerServiceImpl(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     // TODO 2-04 トランザクション管理アノテーションを付加する（伝播属性＝REQUIRED, 読み取り専用＝true）
 
