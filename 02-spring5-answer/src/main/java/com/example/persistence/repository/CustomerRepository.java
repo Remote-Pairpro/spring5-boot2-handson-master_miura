1b725581807daf5839d6464d0bfd7dbfbb487a9d
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-08-26 09:46:04 +0900

    構成変更。自分のリポジトリに上げるため、不要なものを削除、READMEの改変など。

--- 02-spring5-answer/src/main/java/com/example/persistence/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistence.repository;
-
-import com.example.persistence.entity.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-}
